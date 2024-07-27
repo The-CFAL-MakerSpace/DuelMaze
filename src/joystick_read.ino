cb8f89127081c8933307144706a6b55d6060f645
Pratheek <pratheekbhat2008@gmail.com>
2024-07-27 20:00:40 +0530

    first commit ever, gonna add more later

---
+++ src/joystick_read.ino
@@ -0,0 +1,44 @@
+#include <Wire.h>
+#include <Servo.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x26, 16, 2);
+
+const int x = A0;
+const int y = A1;
+const int button = 3;
+
+    Servo s1;
+//this i a porject depicting thedeath of the 
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(x, INPUT);
+    pinMode(y, INPUT);
+    pinMode(button, INPUT_PULLUP);
+    Serial.begin(9600);
+    s1.attach(9);
+    lcd.init();
+    lcd.backlight();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    lcd.setCursor(0, 0);
+    lcd.print("Player 1 Time:");
+    lcd.setCursor(0, 1);
+    // lcd.print(millis() / 1000);
+    int xval = analogRead(x);
+    int yval = analogRead(y);
+    int bval = digitalRead(button);
+
+    int sval = map(xval, 0, 1023, 10, 170);
+    s1.write(sval);
+
+    Serial.print("x = ");
+    Serial.print(xval);
+    Serial.print(" y = ");
+    Serial.print(yval);
+    Serial.print(" button = ");
+    Serial.println(bval);
+    delay(100);
+}
